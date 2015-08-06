a4437b6d88e0d88374db1a0876c25f7b7d73f456
poenarumarius <poenarumariusro@gmail.com>
2015-08-06 16:17:45 +0300

    Update ErrorController.java

--- src/main/java/com/dtr/oas/controller/ErrorController.java
+++ src/main/java/com/dtr/oas/controller/ErrorController.java
@@ -9,12 +9,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ErrorController {
-    static Logger logger = LoggerFactory.getLogger(ErrorController.class);
-    
-    @RequestMapping(value = {"/error/accessDeniedPage"}, method = RequestMethod.GET)
-    public String sendTo403(Model model) {
-        logger.debug("IN: /error/403-GET");
-        return "error/403";
-    }              
+
+	private static final Logger logger = LoggerFactory.getLogger(ErrorController.class);
+
+	@RequestMapping(value = { "/error/accessDeniedPage" }, method = RequestMethod.GET)
+	public String sendTo403(Model model) {
+		if (logger.isDebugEnabled())
+			logger.debug(getClass().getName() + ":sendTo403: /error/403-GET");
+
+		return "error/403";
+	}
 
 }
